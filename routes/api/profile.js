ed89a2584d00924d66202aea91d0cdf4764b22ac
andretong@gmail.com <andretong@gmail.com>
2018-08-30 18:16:35 -0300

    Delete Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -272,4 +272,16 @@ router.post('/', passport.authenticate('jwt', { session : false}),
       .catch(err => console.log(err))
     });
 
+    // @route   DELETE api/profile/
+    // @desc    Delete Profile and User
+    // @access  Private
+    router.delete('/', passport.authenticate('jwt', {session : false}),
+    (req, res) => {
+      Profile.findOneAndRemove({ user : req.user.id})
+        .then(() => {
+          User.findOneAndRemove({ _id: req.user.id})
+            .then(() => res.json({ success : true}))
+        });
+    });
+
 module.exports = router;
