f7058d18f5be9ec44894375ab7e22cb560ac1395
andretong@gmail.com <andretong@gmail.com>
2018-09-06 14:56:26 -0300

    Initial Front

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+import NavBar from './components/layout/NavBar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Landing extends React.Component {
+  render () {
+    return (
+    <div className="landing">
+      <div className="dark-overlay landing-inner text-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center">
+              <h1 className="display-3 mb-4">
+                Developers Connector
+              </h1>
+              <p className="lead">Create a developer Profile / Portafolio</p>
+              <hr />
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default Landing;
